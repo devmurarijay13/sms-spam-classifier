e6ec440e6c2c810b63b7c61e4d306e32670676ba
devmurarijay13 <devmurarijay66@gmail.com>
2025-05-18 22:49:16 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	app.py

--- app.py
+++ app.py
@@ -1,83 +1,12 @@
-# import streamlit as st
-# import pickle
-# import string
-#
-# from nltk.stem.porter import PorterStemmer
-# import nltk
-# nltk.download('punkt')
-# nltk.download('stopwords')
-#
-#
-# from nltk.corpus import stopwords
-#   # Correct resource
-# ps = PorterStemmer()
-# def transform_text(text):
-#     text = text.lower()
-#
-#     text = nltk.word_tokenize(text)  # tokenizer will separate each words
-#
-#     y = []
-#     for i in text:  # removing special chars
-#         if i.isalnum():
-#             y.append(i)
-#
-#     text = y[:]
-#     y.clear()
-#
-#     for i in text:  # removing stop words and punctuation marks
-#         if i not in stopwords.words('english') and i not in string.punctuation:
-#             y.append(i)
-#     text = y[:]
-#     y.clear()
-#
-#     for i in text:
-#         y.append(ps.stem(i))
-#
-#     return " ".join(y)
-#
-# tfidf = pickle.load(open('vectorizer.pkl','rb'))
-# model = pickle.load(open('model.pkl','rb'))
-#
-# st.title("Email/SMS Spam Classifier")
-#
-# input_sms = st.text_area("Enter The Message")
-#
-# if st.button('Predict'):
-#     # preprocess
-#     transform_sms = transform_text(input_sms)
-#
-#     # vectorize
-#     vector_input = tfidf.transform([transform_sms])
-#
-#     # predict
-#     result = model.predict(vector_input)[0]
-#
-#     # display
-#     if result == 1:
-#         st.header("Spam")
-#     else:
-#         st.header("Not Spam")
-
-
 import streamlit as st
 import pickle
 import string
+from nltk.corpus import stopwords
 import nltk
 from nltk.stem.porter import PorterStemmer
-from nltk.corpus import stopwords
-
-# Ensure necessary NLTK data is available
-try:
-    nltk.data.find('tokenizers/punkt')
-except LookupError:
-    nltk.download('punkt',quiet=True)
-
-try:
-    nltk.data.find('corpora/stopwords')
-except LookupError:
-    nltk.download('stopwords')
 
 ps = PorterStemmer()
+
 
 def transform_text(text):
     text = text.lower()
@@ -103,16 +32,23 @@ def transform_text(text):
 
     return " ".join(y)
 
-# Load models
-tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
-model = pickle.load(open('model.pkl', 'rb'))
+tfidf = pickle.load(open('vectorizer.pkl','rb'))
+model = pickle.load(open('model.pkl','rb'))
 
-# Streamlit UI
 st.title("Email/SMS Spam Classifier")
-input_sms = st.text_area("Enter The Message")
+
+input_sms = st.text_area("Enter the message")
 
 if st.button('Predict'):
-    transform_sms = transform_text(input_sms)
-    vector_input = tfidf.transform([transform_sms])
-    result = model.predict([vector_input])[0]
-    st.header("Spam" if result == 1 else "Not Spam")
+
+    # 1. preprocess
+    transformed_sms = transform_text(input_sms)
+    # 2. vectorize
+    vector_input = tfidf.transform([transformed_sms])
+    # 3. predict
+    result = model.predict(vector_input)[0]
+    # 4. Display
+    if result == 1:
+        st.header("Spam")
+    else:
+        st.header("Not Spam")
