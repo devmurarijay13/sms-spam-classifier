017e6ed77086f6fddca12301b329b742c50b1bd8
devmurarijay13 <devmurarijay66@gmail.com>
2025-05-19 15:36:15 +0530

    Prepared for deployment: added model/vectorizer and NLTK fix

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ import streamlit as st
 import pickle
 import string
 from nltk.corpus import stopwords
-import nltk
 from nltk.stem.porter import PorterStemmer
+import nltk
+nltk.download('stopwords')
 
 ps = PorterStemmer()
 
